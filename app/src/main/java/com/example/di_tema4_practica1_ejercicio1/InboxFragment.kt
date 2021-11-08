7f35719d85213a9046a31c5bd74f74420ac09bbc
Cherry708 <aluser8241@ieselcaminas.org>
2021-11-08 20:50:42 +0100

    Ultimo punto de los apuntes con ejemplos: mobile_navigation

---
+++ app/src/main/java/com/example/di_tema4_practica1_ejercicio1/InboxFragment.kt
@@ -0,0 +1,20 @@
+package com.example.di_tema4_practica1_ejercicio1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class InboxFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_inbox, container, false)
+        return root
+
+    }
+}
---
+++ app/src/main/java/com/example/di_tema4_practica1_ejercicio1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.di_tema4_practica1_ejercicio1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
